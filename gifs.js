abcdb13cd81d0841a3261536b530fca11f4a3711
Douglas-Silverman <53188336+Douglas-Silverman@users.noreply.github.com>
2020-03-15 18:40:12 -0400

    Seventh Major Commit

--- gifs.js
+++
@@ -1,9 +0,0 @@
-function runGif(id){
-    let img = document.getElementById(id);
-    let src = img.getAttribute("src");
-    let data_alt = img.getAttribute("data-alt");
-
-    img.setAttribute("src", data_alt);
-    img.setAttribute("data-alt", src);
-    console.log(src);
-}
---
+++ scripts.js
@@ -0,0 +1,27 @@
+function runGif(id){
+    let img = document.getElementById(id);
+    let src = img.getAttribute("src");
+    let data_alt = img.getAttribute("data-alt");
+
+    img.setAttribute("src", data_alt);
+    img.setAttribute("data-alt", src);
+    console.log(src);
+}
+
+function openGithub() {
+    window.open("https://github.com/douglas-silverman");
+}
+
+function openLinkedIn() {
+    window.open("https://linkedin.com/in/douglas-silverman");
+}
+
+function imageTransition(id) {
+    let img = document.getElementById(id);
+    let src = img.getAttribute("src");
+    let data_alt = img.getAttribute("data-alt");
+    setTimeout(function(){ 
+    img.setAttribute("src", data_alt);
+    img.setAttribute("data-alt", src);
+    }, 200);
+}
