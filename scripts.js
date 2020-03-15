b968918e15125a1a0f69b8283b9da4207a03f051
Douglas-Silverman <53188336+Douglas-Silverman@users.noreply.github.com>
2020-03-15 19:34:08 -0400

    preloader commit 2

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
-function runGif(id){
-    let img = document.getElementById(id);
-    let src = img.getAttribute("src");
-    let data_alt = img.getAttribute("data-alt");
+function preloader() {
+     let imgs= [
+        "pictures/326 logos.png",
+        "pictures/Math 456.png",
+        "pictures/Favicon for git.png",
+        "pictures/github logo.png",
+        "pictures/github logo-hover.png",
+        "pictures/linkedIn logo.png",
+        "pictures/linkedIn logo-hover.png",
+        "pictures/Mona_Lisa_Still.png",
+        "pictures/mona-lisa-mosaic.gif",
+        "pictures/profilePic.png",
+        "pictures/strategy pic.png",
+        "pictures/ultimate_group_pic.png",
+        "pictures/xochimilco.gif",
+        "pictures/xochimilco-still.png"];
 
-    img.setAttribute("src", data_alt);
-    img.setAttribute("data-alt", src);
-    console.log(src);
+        let images = [];
+        for (i = 0; i < imgs.length; i++) {
+            images[i] = new Image()
+            images[i].src = imgs[i]
+        }
+        console.log(images);
 }
+
 
 function openGithub() {
     window.open("https://github.com/douglas-silverman");
